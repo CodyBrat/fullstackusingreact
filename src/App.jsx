c64310522d807f81c921590ab74cf6d2dee8cef5
CodyBrat <priyabratasingh2005@gmail.com>
2025-09-14 16:29:00 +0530

    add more impressive components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { BackgroundBeamsDemo} from './components/BackgroundBeamsDemo'
+import { MacbookScrollDemo } from './components/MacbookScrolldemo'  
 import { LampDemo } from './components/LampDemo'
+import friendsImage from './assets/friends2.png'
 
 function App() {
   return (
@@ -13,6 +15,9 @@ function App() {
       <div className="relative z-10">
         <h1 className='Heading'>Doozy</h1>
         {/* Other content goes here */}
+      </div>
+      <div className="transform scale-125">
+        <MacbookScrollDemo />
       </div>
     </div>
   )
