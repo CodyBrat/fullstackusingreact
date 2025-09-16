a172aacdcb39564cf778bc6500d1efc57f9e9dd4
CodyBrat <priyabratasingh2005@gmail.com>
2025-09-16 11:14:29 +0530

    made the heading more better

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,28 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { BackgroundBeamsDemo} from './components/BackgroundBeamsDemo'
 import { MacbookScrollDemo } from './components/MacbookScrolldemo' 
 import { NavbarDemo } from './components/NavbarDemo'
-import { LampDemo } from './components/LampDemo'
 import friendsImage from './assets/friends2.png'
+import  TextPressure  from './components/Textpressure'
 
 function App() {
   return (
     <div className="relative min-h-screen">
       <BackgroundBeamsDemo />
-      <div className="relative z-10">
-        <h1 className='Heading'>Doozy</h1>
+      <div className="relative z-10 flex items-center justify-center h-screen">
+        <div style={{width: '100%', maxWidth: '1200px'}}>
+          <TextPressure
+          text="Doozy"
+          flex={true}
+          alpha={false}
+          stroke={false}
+          width={true}
+          weight={true}
+          italic={true}
+          textColor="#ffffff"
+          strokeColor="#ff0000"
+          minFontSize={36}
+        />
+        </div>
         {/* Other content goes here */}
       </div>
       {/* <div className="transform scale-125">
